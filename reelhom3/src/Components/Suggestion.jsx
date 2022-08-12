4e12ac973ead5eae1087c16e7369c7413920d28e
mugishap <precieuxmugisha@gmail.com>
2022-08-12 13:40:02 +0200

    bugfix:Changed suggestions layout

--- reelhom3/src/Components/Suggestion.jsx
+++ reelhom3/src/Components/Suggestion.jsx
@@ -3,15 +3,17 @@ import { Link } from 'react-router-dom'
 
 function Suggestion(props) {
     return (
-        <div className='flex my-2 items-center justify-center'>
-            <div className="image flex items-center justify-center w-12 h-12">
-                <img class='w-full h-full rounded-full object-cover' src={props.user.profile} alt="" />
+        <div className='flex my-2 items-start  justify-between w-full px-4'>
+            <div className='flex'>
+                <div className="image flex items-center justify-center w-12 h-12">
+                    <img class='w-full h-full rounded-full object-cover' src={props.user.profile} alt="" />
+                </div>
+                <div className='ml-3 flex items-start justify-start h-full flex-col'>
+                    <p className='text-lg font-semibold text-gray whitespace-nowrap'>{props.user.fullname}</p>
+                    <Link to={`account/${props.user._id}`}><p className='text-,d font-light whitespace-nowrap'>@{props.user.username}</p></Link>
+                </div>
             </div>
-            <div className='ml-3 flex items-center justify-start h-full flex-col'>
-                <p className='text-lg font-semibold text-gray whitespace-nowrap'>{props.user.fullname}</p>
-                <Link to={`account/${props.user._id}`}><p className='text-,d font-light whitespace-nowrap'>@{props.user.username}</p></Link>
-            </div>
-            <div className="ml-3 flex items-center justify-center">
+            <div className="ml-3 flex items-center justify-center float-right relative right-0">
                 <Link to={`account/${props.user._id}`}><button className='bg-pink-500 hover:bg-pink-600 text-white font-bold py-2 px-4 rounded-full'>View</button></Link>
             </div>
         </div>
